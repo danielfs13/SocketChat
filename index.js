b7caaa99f80fb10ab109ac01698486d213c84071
danielfs13 <danielstrano13@gmail.com>
2021-12-03 19:33:18 -0300

    Agora o chat exbibe quando o usuário está digitando.

--- index.js
+++ index.js
@@ -4,21 +4,31 @@ const http = require("http");
 const server = http.createServer(app);
 const { Server } = require("socket.io");
 const io = new Server(server);
+let usersOnline = 0;
 
 app.get("/", (req, res) => {
   res.sendFile(__dirname + "/index.html");
 });
 // io = server / socket = usuario
 io.on("connection", (socket) => {
-  io.emit("chat message", "Usuário Conectado");
+  usersOnline++;
+  console.log(socket.id);
+  io.emit("chat message", "Usuário Conectado, online: " + usersOnline);
   console.log("a user connected");
+
   socket.on("disconnect", () => {
-    io.emit("chat message", "Usuário Desconectado");
+    usersOnline--;
+    io.emit("chat message", "Usuário Desconectado online: " + usersOnline);
     console.log("a user disconnected");
   });
+
   socket.on("chat message", (msg) => {
     io.emit("chat message", msg);
     console.log("message: " + msg);
+  });
+
+  socket.on("user typing", () => {
+    socket.broadcast.emit("user typing");
   });
 });
 
