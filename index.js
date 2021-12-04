244059ec915020eddc14246e6cef45c81f633f52
danielfs13 <danielstrano13@gmail.com>
2021-12-04 17:33:40 -0300

    Feature: Nicknames adicionados

--- index.js
+++ index.js
@@ -4,34 +4,54 @@ const http = require("http");
 const server = http.createServer(app);
 const { Server } = require("socket.io");
 const io = new Server(server);
-let usersOnline = 0;
+var usuarios = [];
 
 app.get("/", (req, res) => {
   res.sendFile(__dirname + "/index.html");
 });
 // io = server / socket = usuario
 io.on("connection", (socket) => {
-  usersOnline++;
-  console.log(socket.id);
-  io.emit("chat message", "Usuário Conectado, online: " + usersOnline);
-  console.log("a user connected");
-
-  socket.on("disconnect", () => {
-    usersOnline--;
-    io.emit("chat message", "Usuário Desconectado online: " + usersOnline);
-    console.log("a user disconnected");
+  socket.on("new user", function (data, callback) {
+    const user = usuarios.find((x) => x.nickname === data);
+    if (user) {
+      callback(false);
+    } else {
+      callback(true);
+      const userData = {
+        id: socket.id,
+        nickname: data,
+      };
+      usuarios.push(userData);
+      console.log(usuarios);
+      io.emit(
+        "chat message",
+        data + " acabou de entrar, online: " + usuarios.length
+      );
+    }
   });
 
   socket.on("chat message", (msg) => {
     io.emit("chat message", msg);
-    console.log("message: " + msg);
   });
 
   socket.on("user typing", () => {
     socket.broadcast.emit("user typing");
   });
+
+  socket.on("disconnect", () => {
+    const user = usuarios.find((x) => x.id === socket.id);
+    console.log(user);
+    usuarios.splice(usuarios.indexOf(user), 1);
+    console.log(usuarios);
+    if (user && user.nickname) {
+      io.emit(
+        "chat message",
+        user.nickname + " desconectado, online: " + usuarios.length
+      );
+    }
+  });
 });
 
 server.listen(3000, () => {
-  console.log("listening on *:3000");
+  console.log("Servidor iniciado.");
 });
